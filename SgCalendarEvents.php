41eb8e5caef6ee7951e3c50eecbbe9871ee5bf0c
stwe <admin@goretzki.eu>
2013-06-22 19:42:52 +0200

    add events

--- SgCalendarEvents.php
+++ SgCalendarEvents.php
@@ -10,14 +10,28 @@ namespace Sg\CalendarBundle;
 final class SgCalendarEvents
 {
     /**
-     * EVENT_CREATE_COMPLETED is called every time when a new event has been saved.
+     * EVENT_CREATE_SUCCESS occurs when the event creation form is submitted successfully.
+     *
+     * @var string
+     */
+    const EVENT_CREATE_SUCCESS = 'sg_calendar.event.create.success';
+
+    /**
+     * EVENT_UPDATE_SUCCESS occurs when the event update form is submitted successfully.
+     *
+     * @var string
+     */
+    const EVENT_UPDATE_SUCCESS = 'sg_calendar.event.update.success';
+
+    /**
+     * EVENT_CREATE_COMPLETED occurs after saving the event in the creation process.
      *
      * @var string
      */
     const EVENT_CREATE_COMPLETED = 'sg_calendar.event.create.completed';
 
     /**
-     * EVENT_CREATE_COMPLETED is called every time when a new event has been updated.
+     * EVENT_CREATE_COMPLETED occurs after saving the event in the update process.
      *
      * @var string
      */
