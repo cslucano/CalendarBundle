950bb3e43756bff5e6d474dd70e711b3244fa04f
stwe <admin@goretzki.eu>
2013-07-06 14:12:31 +0200

    add calendar form factory

---
+++ Form/Factory/CalendarFormFactory.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Sg\CalendarBundle\Form\Factory;
+
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormTypeInterface;
+
+/**
+ * Class CalendarFormFactory
+ *
+ * @package Sg\CalendarBundle\Form\Factory
+ */
+class CalendarFormFactory implements CalendarFormFactoryInterface
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var string|integer The name of the form
+     */
+    private $name;
+
+    /**
+     * @var string|FormTypeInterface The type of the form
+     */
+    private $type;
+
+
+    //-------------------------------------------------
+    // Ctor.
+    //-------------------------------------------------
+
+    /**
+     * Ctor.
+     *
+     * @param FormFactoryInterface     $formFactory A FormFactoryInterface instance
+     * @param string|integer           $name        The name of the form
+     * @param string|FormTypeInterface $type        The type of the form
+     */
+    public function __construct(FormFactoryInterface $formFactory, $name, $type)
+    {
+        $this->formFactory = $formFactory;
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+
+    //-------------------------------------------------
+    // CalendarFormFactoryInterface
+    //-------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createForm($data = null, array $options = array())
+    {
+        return $this->formFactory->createNamed($this->name, $this->type, $data, $options);
+    }
+}
