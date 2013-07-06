6ee39ed8ce7124873245fa00d4ae0b14c066a9e1
stwe <admin@goretzki.eu>
2013-07-06 16:29:46 +0200

    calendar crud added

--- Controller/CalendarController.php
+++ Controller/CalendarController.php
@@ -2,6 +2,7 @@
 
 namespace Sg\CalendarBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -17,6 +18,10 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
  */
 class CalendarController extends Controller
 {
+    //-------------------------------------------------
+    // Actions
+    //-------------------------------------------------
+
     /**
      * Shows the calendar.
      *
@@ -33,5 +38,249 @@ class CalendarController extends Controller
             'get_xhr_events_url' => $this->generateUrl('sg_calendar_get_xhr_events'),
             'update_xhr_event_url' => $this->generateUrl('sg_calendar_update_xhr_event')
         );
+    }
+
+    /**
+     * Creates a new Calendar entity.
+     *
+     * @param Request $request
+     *
+     * @Route("/create", name="sg_calendar_create_calendar")
+     * @Method("POST")
+     * @Template("SgCalendarBundle:Calendar:new.html.twig")
+     * @ApiDoc()
+     *
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function postAction(Request $request)
+    {
+        $calendar = $this->getCalendarManager()->newCalendar();
+
+        $form = $this->getCalendarFormFactory()->createForm($calendar);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->getCalendarManager()->updateCalendar($calendar);
+
+            return $this->redirect($this->generateUrl('sg_calendar_get_calendar', array('id' => $calendar->getId())));
+        }
+
+        return array(
+            'entity' => $calendar,
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Calendar entity.
+     *
+     * @Route("/new", name="sg_calendar_new_calendar")
+     * @Method("GET")
+     * @Template()
+     * @ApiDoc()
+     *
+     * @return array
+     */
+    public function newAction()
+    {
+        $calendar = $this->getCalendarManager()->newCalendar();
+
+        $form = $this->getCalendarFormFactory()->createForm($calendar);
+
+        return array(
+            'entity' => $calendar,
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Calendar entity.
+     *
+     * @param integer $id The entity id
+     *
+     * @Route("/{id}/show", name="sg_calendar_get_calendar")
+     * @Method("GET")
+     * @Template()
+     * @ApiDoc()
+     *
+     * @return array
+     */
+    public function getAction($id)
+    {
+        $calendar = $this->getCalendarById($id);
+
+        return array(
+            'entity' => $calendar
+        );
+    }
+
+    /**
+     * Displays a form to update an existing Calendar entity.
+     *
+     * @param integer $id The entity id
+     *
+     * @Route("/{id}/edit", name="sg_calendar_edit_calendar")
+     * @Method("GET")
+     * @Template()
+     * @ApiDoc()
+     *
+     * @return array
+     */
+    public function editAction($id)
+    {
+        $calendar = $this->getCalendarById($id);
+
+        $editForm = $this->getCalendarFormFactory()->createForm($calendar, array('method' => 'PUT'));
+
+        return array(
+            'entity' => $calendar,
+            'edit_form' => $editForm->createView()
+        );
+    }
+
+    /**
+     * Updates an existing Calendar entity.
+     *
+     * @param Request $request A Request instance
+     * @param integer $id      The entity id
+     *
+     * @Route("/{id}/update", name="sg_calendar_update_calendar")
+     * @Method("PUT")
+     * @Template("SgCalendarBundle:Calendar:edit.html.twig")
+     * @ApiDoc()
+     *
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function putAction(Request $request, $id)
+    {
+        $calendar = $this->getCalendarById($id);
+
+        $editForm = $this->getCalendarFormFactory()->createForm($calendar, array('method' => 'PUT'));
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $this->getCalendarManager()->updateCalendar($calendar);
+
+            return $this->redirect($this->generateUrl('sg_calendar_get_calendar', array('id' => $id)));
+        }
+
+        return array(
+            'entity' => $calendar,
+            'edit_form' => $editForm->createView()
+        );
+    }
+
+    /**
+     * Displays a form to delete an existing Calendar entity.
+     *
+     * @param integer $id The entity id
+     *
+     * @Route("/{id}/remove", name="sg_calendar_remove_calendar")
+     * @Method("GET")
+     * @Template()
+     * @ApiDoc()
+     *
+     * @return array
+     */
+    public function removeAction($id)
+    {
+        $calendar = $this->getCalendarById($id);
+
+        $removeForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity' => $calendar,
+            'remove_form' => $removeForm->createView()
+        );
+    }
+
+    /**
+     * Deletes an existing Calendar entity.
+     *
+     * @param Request $request A Request instance
+     * @param integer $id      The entity id
+     *
+     * @Route("/{id}/delete", name="sg_calendar_delete_calendar")
+     * @Method("DELETE")
+     * @ApiDoc()
+     *
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $calendar = $this->getCalendarById($id);
+
+        $removeForm = $this->createDeleteForm($id);
+        $removeForm->handleRequest($request);
+
+        if ($removeForm->isValid()) {
+            $this->getCalendarManager()->removeCalendar($calendar);
+
+            return $this->redirect($this->generateUrl('sg_calendar'));
+        }
+
+        return array(
+            'entity' => $calendar,
+            'remove_form' => $removeForm->createView()
+        );
+    }
+
+
+    //-------------------------------------------------
+    // Private
+    //-------------------------------------------------
+
+    /**
+     * Return an calendar by id.
+     *
+     * @param integer $id
+     *
+     * @return \Sg\CalendarBundle\Model\CalendarInterface
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    private function getCalendarById($id)
+    {
+        $calendar = $this->getCalendarManager()->findCalendarBy(array('id' => $id));
+        if (!$calendar) {
+            throw $this->createNotFoundException('Unable to find Calendar entity.');
+        }
+
+        return $calendar;
+    }
+
+    /**
+     * Creates a form to delete a Calendar entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+
+
+    //-------------------------------------------------
+    // Services
+    //-------------------------------------------------
+
+    /**
+     * @return \Sg\CalendarBundle\Model\CalendarManagerInterface
+     */
+    private function getCalendarManager()
+    {
+        return $this->container->get('sg_calendar.calendar_manager');
+    }
+
+    /**
+     * @return \Sg\CalendarBundle\Form\Factory\CalendarFormFactoryInterface
+     */
+    private function getCalendarFormFactory()
+    {
+        return $this->container->get('sg_calendar.form_factory.calendar');
     }
 }
