6aa6c06039092d91742d26c18cbc4ffbd024ed7e
stwe <stegowe@gmail.com>
2013-07-11 15:52:15 +0200

    New feature: DatePicker and DateTimePicker Js

---
+++ Resources/public/js/sgcalendar.js
@@ -0,0 +1,67 @@
+var sg_calendar_datepicker = (function() {
+
+    var locale = "";
+
+    return {
+
+        initLocale : function() {
+            if(global.locale) {
+                locale = global.locale;
+                console.log(locale)
+            }
+            else {
+                console.error('The locale is missing, default locale will be set (en)');
+                locale = "en";
+            }
+        },
+
+        getLocale : function(length) {
+            if(length == 2){
+                return locale.split('_')[0];
+            }
+            return locale;
+        },
+
+        initDatePicker : function() {
+
+            if($.datepicker.regional[this.getLocale(4)] != undefined) {
+                $.datepicker.setDefaults( $.datepicker.regional[this.getLocale(4)] );
+            } else if($.datepicker.regional[this.getLocale(2)] != undefined) {
+                $.datepicker.setDefaults( $.datepicker.regional[this.getLocale(2) ] );
+            } else {
+                $.datepicker.setDefaults( $.datepicker.regional['']);
+            }
+
+            $('.SgCalendarDatePicker').each(function() {
+                var id_input=this.id.split('_datepicker')[0];
+                var sfInput = $('#'+id_input)[0];
+                if(! (sfInput)) {
+                    console.error('An error has occurred while creating the datepicker.');
+                }
+                $(this).datepicker({
+                    'yearRange':$(this).data('yearrange'),
+                    'changeMonth':$(this).data('changemonth'),
+                    'changeYear':$(this).data('changeyear'),
+                    'altField' : '#'+id_input,
+                    'altFormat' : 'yy-mm-dd',
+                    'minDate' : null,
+                    'maxDate': null
+                });
+
+                $(this).keyup(function(e) {
+                    if(e.keyCode == 8 || e.keyCode == 46) {
+                        $.datepicker._clearDate(this);
+                        $('#'+id_input)[0].value = '';
+                    }
+                });
+                var dateSf = $.datepicker.parseDate('yy-mm-dd',sfInput.value);
+
+                $(this).datepicker('setDate',dateSf);
+                $(this).show();
+                $(sfInput).hide();
+            })
+        }
+
+    };
+
+}());
