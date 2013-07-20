3fb4a092c804f31311598afef4ee3e6c5263d3c4
stwe <stegowe@gmail.com>
2013-07-21 00:11:51 +0200

    Fixtures added

---
+++ DataFixtures/ORM/Data.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Sg\CalendarBundle\DataFixtures\ORM;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Fixtures
+ */
+class Data extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        $calendar = $this->getCalendarManager()->newCalendar();
+        $calendar->setName('UserCalendar');
+        $calendar->setCreatedAt(new \DateTime());
+        $calendar->setUpdatedAt(new \DateTime());
+        $calendar->setCreatedBy($this->getReference('user'));
+        $calendar->setUpdatedBy($this->getReference('user'));
+
+        $event = $this->getEventManager()->newEvent();
+        $event->setCalendar($calendar);
+        $event->setTitle('UserTestevent');
+        $event->setStart(new \DateTime());
+        $event->setAllDay(true);
+        $event->setEnd(null);
+        $event->setCreatedAt(new \DateTime());
+        $event->setUpdatedAt(new \DateTime());
+        $event->setCreatedBy($this->getReference('user'));
+        $event->setUpdatedBy($this->getReference('user'));
+
+        $manager->persist($calendar);
+        $manager->persist($event);
+
+        $manager->flush();
+    }
+
+    /**
+     * @return \Sg\CalendarBundle\Model\CalendarManagerInterface
+     */
+    private function getCalendarManager()
+    {
+        return $this->container->get('sg_calendar.calendar_manager');
+    }
+
+    /**
+     * @return \Sg\CalendarBundle\Model\EventManagerInterface
+     */
+    protected function getEventManager()
+    {
+        return $this->container->get('sg_calendar.event_manager');
+    }
+}
