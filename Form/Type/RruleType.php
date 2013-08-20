fabdaf06e625278309d40b27b764571b1d9a3b44
stwe <stegowe@gmail.com>
2013-08-20 14:23:17 +0200

    update translation

--- Form/Type/RruleType.php
+++ Form/Type/RruleType.php
@@ -34,47 +34,47 @@ class RruleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('rule', null, array('label' => 'RRule'))
+            ->add('rule', null, array('label' => 'calendar.entity.rrule.rule', 'translation_domain' => 'messages'))
             ->add('freq', 'choice', array(
                     'choices' => array(
-                        RruleInterface::DAILY => 'calendar.period.daily',
-                        RruleInterface::WEEKLY => 'calendar.period.weekly',
-                        RruleInterface::MONTHLY => 'calendar.period.monthly',
-                        RruleInterface::YEARLY => 'calendar.period.yearly',
-                        RruleInterface::HOURLY => 'Stündlich',
-                        RruleInterface::MINUTELY => 'Minütlich',
-                        RruleInterface::SECONDLY => 'Sekündlich'
+                        RruleInterface::DAILY => 'calendar.frequency.daily',
+                        RruleInterface::WEEKLY => 'calendar.frequency.weekly',
+                        RruleInterface::MONTHLY => 'calendar.frequency.monthly',
+                        RruleInterface::YEARLY => 'calendar.frequency.yearly',
+                        RruleInterface::HOURLY => 'calendar.frequency.hourly',
+                        RruleInterface::MINUTELY => 'calendar.frequency.minutely',
+                        RruleInterface::SECONDLY => 'calendar.frequency.secondly'
                     ),
-                    'label' => 'calendar.entity.recurrence.period',
+                    'label' => 'calendar.entity.rrule.freq',
                     'translation_domain' => 'messages'
                 ))
-            ->add('dtstart', 'datePicker', array('label' => 'Start', 'translation_domain' => 'messages'))
-            ->add('until', 'datePicker', array('label' => 'calendar.entity.recurrence.end', 'translation_domain' => 'messages'))
-            ->add('count', null, array('label' => 'Wiederholungen'))
-            ->add('ival', null, array('label' => 'Interval'))
-            ->add('wkst', null, array('label' => 'Wochenstart'))
+            ->add('dtstart', 'datePicker', array('label' => 'calendar.entity.rrule.dtstart', 'translation_domain' => 'messages'))
+            ->add('until', 'datePicker', array('label' => 'calendar.entity.rrule.until', 'translation_domain' => 'messages'))
+            ->add('count', null, array('label' => 'calendar.entity.rrule.count', 'translation_domain' => 'messages'))
+            ->add('ival', null, array('label' => 'calendar.entity.rrule.ival', 'translation_domain' => 'messages'))
+            ->add('wkst', null, array('label' => 'calendar.entity.rrule.wkst', 'translation_domain' => 'messages'))
             ->add('byweekday', 'choice', array(
                     'multiple' => true,
                     'choices'  => array(
-                        RruleInterface::MONDAY => 'Montag',
-                        RruleInterface::TUESDAY => 'Dienstag',
-                        RruleInterface::WEDNESDAY => 'Mittwoch',
-                        RruleInterface::THURSDAY => 'Donnerstag',
-                        RruleInterface::FRIDAY => 'Freitag',
-                        RruleInterface::SATURDAY => 'Samstag',
-                        RruleInterface::SUNDAY => 'Sonntag'
+                        RruleInterface::MONDAY => 'calendar.fullcalendar.day.short.mon',
+                        RruleInterface::TUESDAY => 'calendar.fullcalendar.day.short.tue',
+                        RruleInterface::WEDNESDAY => 'calendar.fullcalendar.day.short.wed',
+                        RruleInterface::THURSDAY => 'calendar.fullcalendar.day.short.thu',
+                        RruleInterface::FRIDAY => 'calendar.fullcalendar.day.short.fri',
+                        RruleInterface::SATURDAY => 'calendar.fullcalendar.day.short.sat',
+                        RruleInterface::SUNDAY => 'calendar.fullcalendar.day.short.sun'
                     ),
-                    'label' => 'Wochentage',
+                    'label' => 'calendar.entity.rrule.byweekday',
                     'translation_domain' => 'messages'
                 ))
-            ->add('bymonth', null, array('label' => 'ByMonth'))
-            ->add('bysetpos', null, array('label' => 'BySetpos'))
-            ->add('bymonthday', null, array('label' => 'ByMonthday'))
-            ->add('byyearday', null, array('label' => 'Byyearday'))
-            ->add('byweekno', null, array('label' => 'ByWeekno'))
-            ->add('byhour', null, array('label' => 'Byhour'))
-            ->add('byminute', null, array('label' => 'Byminute'))
-            ->add('bysecond', null, array('label' => 'Bysecond'));
+            ->add('bymonth', null, array('label' => 'calendar.entity.rrule.bymonth', 'translation_domain' => 'messages'))
+            ->add('bysetpos', null, array('label' => 'calendar.entity.rrule.bysetpos', 'translation_domain' => 'messages'))
+            ->add('bymonthday', null, array('label' => 'calendar.entity.rrule.bymonthday', 'translation_domain' => 'messages'))
+            ->add('byyearday', null, array('label' => 'calendar.entity.rrule.byyearday', 'translation_domain' => 'messages'))
+            ->add('byweekno', null, array('label' => 'calendar.entity.rrule.byweekno', 'translation_domain' => 'messages'))
+            ->add('byhour', null, array('label' => 'calendar.entity.rrule.byhour', 'translation_domain' => 'messages'))
+            ->add('byminute', null, array('label' => 'calendar.entity.rrule.byminute', 'translation_domain' => 'messages'))
+            ->add('bysecond', null, array('label' => 'calendar.entity.rrule.bysecond', 'translation_domain' => 'messages'));
     }
 
     /**
