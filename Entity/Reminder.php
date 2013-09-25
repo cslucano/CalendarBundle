ceba92eb0e9ac8196be3fdcde6da5b549e046479
stwe <stegowe@gmail.com>
2013-09-25 19:47:47 +0200

    add Reminder default entity

---
+++ Entity/Reminder.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the SgCalendarBundle package.
+ *
+ * (c) stwe <https://github.com/stwe/CalendarBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sg\CalendarBundle\Entity;
+
+use Sg\CalendarBundle\Model\AbstractReminder as BaseReminder;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Reminder
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ *
+ * @package Sg\CalendarBundle\Entity
+ */
+class Reminder extends BaseReminder
+{
+    /**
+     * Ctor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        // your own logic
+    }
+}
