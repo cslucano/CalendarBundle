7cd2b3495fe4f36f2ab1fc49e14983fdadc33190
stwe <stegowe@gmail.com>
2013-11-17 00:09:05 +0100

    LocaleSubscriber

---
+++ Subscriber/LocaleSubscriber.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * This file is part of the SgCalendarBundle package.
+ *
+ * (c) stwe <https://github.com/stwe/CalendarBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sg\CalendarBundle\Subscriber;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class LocaleSubscriber
+ *
+ * @package Sg\CalendarBundle\Subscriber
+ */
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+
+
+    /**
+     * Ctor.
+     *
+     * @param string $defaultLocale
+     */
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+
+    //-------------------------------------------------
+    // EventSubscriberInterface
+    //-------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
+    }
+} 
