d4bf6aa20abf77733353f1d5ff52a1808f5e2ddc
stwe <stegowe@gmail.com>
2013-11-29 21:58:47 +0100

    fix calendar choice

--- Form/Type/EventType.php
+++ Form/Type/EventType.php
@@ -11,10 +11,13 @@
 
 namespace Sg\CalendarBundle\Form\Type;
 
+use Sg\CalendarBundle\Doctrine\CalendarManager;
+use Sg\CalendarBundle\Model\EventInterface;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Sg\CalendarBundle\Model\EventInterface;
+use Symfony\Component\Security\Core\SecurityContext;
 
 /**
  * Class EventType
@@ -26,15 +29,39 @@ class EventType extends AbstractType
     /**
      * @var string
      */
-    private $class;
+    private $eventClass;
+
+    /**
+     * @var string
+     */
+    private $calendarClass;
+
+    /**
+     * @var SecurityContext
+     */
+    private $securityContext;
+
+    /**
+     * @var CalendarManager
+     */
+    private $calendarManager;
 
 
     /**
-     * @param string $class The Event class name
+     * @param string          $eventClass      The Event class name
+     * @param string          $calendarClass   The Calendar class name
+     * @param SecurityContext $securityContext The security context
+     * @param CalendarManager $calendarManager The calendar manager
      */
-    public function __construct($class)
+    public function __construct($eventClass, $calendarClass, SecurityContext $securityContext, CalendarManager $calendarManager)
     {
-        $this->class = $class;
+        $this->eventClass = $eventClass;
+
+        //var_dump($calendarClass);die();
+
+        $this->calendarClass = $calendarClass;
+        $this->securityContext = $securityContext;
+        $this->calendarManager = $calendarManager;
     }
 
     /**
@@ -42,13 +69,20 @@ class EventType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $this->securityContext->getToken()->getUser();
+        $userCalendars = $this->calendarManager->findCalendarsByUser($user);
+
         $builder
             ->add('title', null, array('label' => 'calendar.entity.event.title', 'translation_domain' => 'messages'))
             ->add('allDay', null, array('label' => 'calendar.entity.event.allDay', 'translation_domain' => 'messages'))
             ->add('start', 'dateTimePicker', array('label' => 'calendar.entity.event.start', 'translation_domain' => 'messages'))
             ->add('end', 'dateTimePicker', array('label' => 'calendar.entity.event.end', 'translation_domain' => 'messages', 'required' => false))
             ->add('description', null, array('label' => 'calendar.entity.event.description', 'translation_domain' => 'messages'))
-            ->add('calendar', null, array('label' => 'calendar.entity.event.calendar', 'translation_domain' => 'messages'))
+            ->add('calendar', 'entity', array(
+                    'label' => 'calendar.entity.event.calendar', 'translation_domain' => 'messages',
+                    'class' => $this->calendarClass,
+                    'choices' => $userCalendars
+                ))
             ->add('url', null, array('label' => 'calendar.entity.event.url', 'translation_domain' => 'messages'))
             ->add('className', null, array('label' => 'calendar.entity.event.className', 'translation_domain' => 'messages'))
             ->add('editable', null, array('label' => 'calendar.entity.event.editable', 'translation_domain' => 'messages'))
@@ -82,7 +116,7 @@ class EventType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => $this->class
+            'data_class' => $this->eventClass
         ));
     }
 
